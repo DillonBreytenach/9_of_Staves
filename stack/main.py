9d6c474efa73f417f0af329ff241a89ca9249fdc
DillonBreytanbach <DillonB_ach@yahoo.com>
2022-03-31 12:59:58 +0200

    Working_Stack

--- stack/main.py
+++ stack/main.py
@@ -1,7 +1,10 @@
 #LOGIN SCREEN
 #BASE IMPORT
-from lib2to3.pytree import convert
+from threading import Thread
+import threading
 from kivy.app import App
+import sys
+from _thread import *
 
 #UIX IMOPORTS
 from kivy.uix.anchorlayout import AnchorLayout
@@ -15,6 +18,7 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
 from kivy.properties import StringProperty
+from kivy.clock import Clock
 
 #CONNECTION__
 import socket
@@ -36,26 +40,63 @@ class Second_Box(ScrollView):
         except Exception as e:
             print(e)
         self.host = '127.0.0.1'
-        self.port = 7000
+        self.port = 8080
         self.data = ""
         self.text_in = StringProperty("")
+        self.encap = "CLIENT_1@"
+
+        
+        
         try:
             self.sock.connect((self.host, self.port))
+            print("[CONNECTED]")
         except Exception as e:
             print(e)
-        #self.sock.bind(self.host, self.port)
         try:
-            self.sock.listen()
+            self.sock.bind((self.host, self.port))
+            print("BOUND")
+        except Exception as e:
+            print("SOCKET NOT BOUND : ",str(e))
+            
+        try:
+            self.sock.listen(5)
+            self.sock.setblocking(False)
         except Exception as e:
             print(e)    
-    def get_msg(self):
-        try:
-            self.received = self.sock.recv(1024).decode()
-        except Exception as e:
-            print(e)
-            # self.sock.close()
+    received = ""
+        
+        
+    def update(self):
+        
+        t = threading.Thread(target=self.get_msg)
+        t.daemon = True
+        t.start()
+
+
+    def check_data(self, data):
+
+        print("CHECKING DATA: ", str(data))
+        pass
+
+            
+    def get_msg(self, **kwargs):
+        while True:
+            received = ""
+            try:
+                received = self.sock.recv(1024 * 3).decode()
+                print("RECV: ", str(received))
+                self.check_data(received)
+                self.ids.in_com.text = received
+            except Exception as e:
+                print(str(e))
+            if not received:
+                print("MSG:", str(self.get_msg()))
+                break
+            else:
+                return str(self.received)
+        self.sock.close()
+                
     
-        return str(self.received)
     
     def send_msg(self):
         try:
@@ -65,127 +106,42 @@ class Second_Box(ScrollView):
             print(e)
         return str(self.get_msg())
     
-        
+    
     #-----------END OF CONNECTION---
 
     #CONVERT DATA
     def convert(self):
-        self.data = str(self.text_in)
+        self.data = str(self.encap + self.text_in)
         return self.data
 
     #--------------------------------
 
-    def get_text(self):
-        print(self.received)
-        self.ids.in_com.text = self.received
-        return str(self.received)
-
     
     def on_click(self):
         try:
+            #self.set_text(widget)
             print("[BUTTON_CLICKED]")  
             print("TEXT: " + str(self.text_in))
             self.send_msg()
-            
-            self.ids.in_com.text = self.received
+
         except Exception as e:    
             print(e)
+
      #VARS
     
     #FUNCTION defs
     def set_text(self, widget):
         self.text_in = widget.text
+        
         print("[MSG]: " + self.text_in)
-
-
+        self.data = str(self.text_in)
+        
 
 
 class PageLayout_(PageLayout):
     pass
 
 
-"""
-class Login_Widget(BoxLayout):
-    try:
-        def __init__(self, **kwargs):
-            super().__init__(**kwargs)
-            self.orientation = 'vertical'
-        
-    except Exception as e:
-        print(e)
-    
-    
-    
-
-    
-    #CONNECTION
-class BoxWidget(BoxLayout):
-    s = socket.socket()
-    host = '127.0.0.1'
-    port = 7000
-    display = ObjectProperty()
-
-
-    def connect_to_server(self):
-        # called by a Button press
-
-        # Connects to the server
-        self.s.connect((self.host, self.port)) 
-
-        # Receives confirmation from Server
-        data = self.s.recv(1024).decode()      
-
-        # Converts confirmation to string
-        strdata = str(data)                     
-
-        # Prints confirmation
-        print(strdata)                                   
-
-    def send_message(self):    
-        # Is called by the function below
-        # Encodes and sends the message variable                  
-        self.s.send(self.message.encode()) 
-
-        # Waits for a reply   
-        self.receive_message()                     
-
-    def message_to_send(self):  
-        # Defines Message to send                 
-        self.message = self.display.text
-        # Calls function to send the message                
-        self.send_message()     
-
-    # Note
-    # When I used message = input directly in send_message,
-    # the app would crash. So I defined message input 
-    # in its own function which then calls the 
-    # send function  
-
-    # message_to_send is the function actually
-    # called by a button press which then
-    # starts the chain of events
-    # Define Message, Send Message, get Reply
-
-    def receive_message(self):
-        # Decodes a reply                    
-        reply = self.s.recv(1024).decode()
-
-        # Converts reply to a str
-        strreply = str(reply)
-
-        # prints reply
-        print(strreply)
-"""        
-        
-        
-
-"""
-
-class ServerApp(App):    
-     def build(self):
-          box = BoxWidget()
-          return box
-"""
 
 class Main(App):
     pass
--- stack/server.py
+++ stack/server.py
@@ -1,34 +1,115 @@
+from multiprocessing.connection import Client
 import socket
+from _thread import *
+from threading import Thread, ThreadError
+#from kivy.clock import Clock
 
-def Main():
-    host = '127.0.0.1'
-    port = 7000
 
-    mySocket = socket.socket()
-    mySocket.bind((host,port))
+class server():
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.CLient_List = set()
+        self.msg = []
+    
+    def handle_client(self, conn, addr, mySocket):
+        print("[ADDR]: " + str(addr))
+    
+        while True:
+            data = conn.recv(1024 * 3).decode()
+            print("DATA: ", str(data))
 
-    mySocket.listen(10)
 
-    print("ATTEMPTING: ")
-    conn, addr = mySocket.accept()
-    print ("Connection from: " + str(addr))
-    message = 'Thank you connecting'
-    conn.send(message.encode())
+            try:
+                if str(data) == "DISCONN":
+                    #print("FUCK")
+                    mySocket.close()
+            except Exception as e:
+                print("CONN NOT CLOSED", str(e))
 
-    while True:
-        data = conn.recv(1024).decode()
-        strdata = str(data)
-        test_str = str(strdata)+"HELLO"
+
+
+
+            if not data:
+                print("NO DATA YET")
+                return
+            else:
+                
+                try:
+
+                    m_len = len(self.msg)
+                    self.msg.append(str(data))
+                    sending = self.msg[(m_len)]
+
+
+                    try:
+                        for cs in self.CLient_List:
+                            print("cs: ", cs)
+                            
+                            cs.send(sending.encode())
+                    
+                        print("[MSG_SENT]: ", sending)
+                    except Exception as e:
+                        print("[SEND_ALL_FAIL] : ", str(e))
+                except Exception as e:
+                    print("CLIENT_HANDLE:", str(e))    
+
+
+
+
+    
+    def handle_file():
+        pass
+
+
+    def Main(self):
+        host = '127.0.0.1'
+        port = 8080
+        
+        
+        addr = (host, port)
+        mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+       
+        Thread_Count = 0
+        print("SERVER RUNING: ")
+        
         try:
-            conn.sendall(test_str.encode())
-            print(strdata)
+            mySocket.bind(('', port))
+            print("BINDING: ", mySocket)
         except Exception as e:
-            print(e)    
+            print("NOT BINDING: ", str(e))
+        
+        mySocket.listen(40)
+        print("SERVER LISTENING:")
+        
+        while True:
+            try:
+                conn, addr = mySocket.accept()
 
-        try:
-            mySocket.close()
-        except Exception as e:
-            print(e)
+                self.CLient_List.add(conn)
+                print ("Connection from: " + str(addr))
+                print(":LIST:", self.CLient_List)
+            except socket.error as e:
+                print("[ERROR_CONNECTING_NEW_CLIENT] :", str(e))        
+            
+            
+   
+            if addr:
+                Thread_Count += 1
+                print("[THREADS]: ", Thread_Count)
+            
+            try:
+                # start a new thread that listens for each client's messages
+                t = Thread(target=self.handle_client, args=(conn, addr, mySocket))
+                # make the thread daemon so it ends whenever the main thread ends
+                t.daemon = True
+                # start the thread
+                t.start()
+                
+               
+            except ThreadError as e:
+                print(e)
+
 
 if __name__ == '__main__':
-    Main()
+    s = server()
+    s.Main()
